8bf6836ae57010e391f7f2c2477d6f020b71fc56
Mateus Arruda Navarro Albuquerque <mateusnavarro@2ndmarket.com.br>
2022-09-27 16:50:00 -0300

    Protege o contrato com o modifier nonReentrancy

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -2,21 +2,33 @@
 pragma solidity ^0.8.2;
 
 contract Store{
-
+    bool internal locked;
     mapping(address=>uint256) balances;
+    constructor(){
+        locked = false;
+    }
+    modifier nonReentrancy(){
+        require(!locked, "No reentrancy allowed!");
+        locked = true;
+        _;
+        locked = false;
+    }
+    
 
     function Depositar() public payable{
                 
-        balances[msg.sender] = msg.value;
+        balances[msg.sender] += msg.value;
 
     }
 
-    function Saque() public{
+    function Saque() public nonReentrancy{
         require(balances[msg.sender]>0,"Saldo insuficiente para sacar o dinheiro");
 
         (bool success,) = payable(msg.sender).call{value:balances[msg.sender]}("");
 
         require(success,"Houve um erro");
+
+        balances[msg.sender] = 0;
     }
 
 }
